f3a690e8d0f01191bd3c61acdfa77c0157dabbcd
Alexotieno1717 <alexotieno900@gmail.com>
2022-10-13 10:28:43 +0300

    Created utils folder and alert file for functionality

---
+++ src/utils/alerts.jsx
@@ -0,0 +1,36 @@
+// react toastify alert
+import { AxiosError } from "axios";
+import 'react-toastify/dist/ReactToastify.css';
+import { toast, ToastContainer } from "react-toastify";
+
+// toast
+export const SuccessAlert = (message) => {
+  if (message) {
+
+    toast.success(`${message}!  :You are logged in`, {
+      position: toast.POSITION.TOP_RIGHT
+    });
+
+  } else {
+
+    toast.success("Successful! You are logged in", {
+      position: toast.POSITION.TOP_RIGHT
+    });
+
+  }
+}
+
+export const ValidationAlert = (message) => {
+  toast.error(`Filled are required`, {
+    position: toast.POSITION.TOP_RIGHT
+  });
+}
+
+export const ErrorAlert = () => {
+    toast.error(`You don't have an account! redirecting to Register page`, {
+      position: toast.POSITION.TOP_RIGHT
+    });
+
+}
+
+export const AlertContainer = () => ( <ToastContainer autoClose={5000}/> )
