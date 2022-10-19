897e80ca4df3cab197fadc198a759764ab718b1e
Alexotieno1717 <alexotieno900@gmail.com>
2022-10-19 14:06:52 +0300

    Added link to navigate to different component

--- src/components/Navigation/sidebar/SideBar.jsx
+++ src/components/Navigation/sidebar/SideBar.jsx
@@ -55,18 +55,62 @@ function SideBar() {
               <i className='fas fa-users fa-fw me-3'/><span>Leaderboard</span>
             </NavLink>
 
-            <a href="/leaderboard" className="list-group-item list-group-item-action py-2 ripple"
-            ><i className='fas fa-user-plus fa-fw me-3'/><span>My Team</span></a
+            <NavLink to='/my-team'
+                     aria-current="true"
+                     className={({ isActive, isPending }) =>
+                       isActive
+                         ? "active01"
+                         :isPending
+                         ? "pending"
+                         : "list-group-item list-group-item-action py-2 ripple"
+                     }
+
             >
-            <a href="http://localhost:3000/" className="list-group-item list-group-item-action py-2 ripple">
+              <i className='fas fa-user-plus fa-fw me-3'/><span>My Team</span>
+            </NavLink>
+
+            <NavLink to='/support'
+                     aria-current="true"
+                     className={({ isActive, isPending }) =>
+                       isActive
+                         ? "active01"
+                         :isPending
+                         ? "pending"
+                         : "list-group-item list-group-item-action py-2 ripple"
+                     }
+
+            >
               <i className='fas fa-headset fa-fw me-3'/><span>Support</span>
-            </a>
-            <a href="http://localhost:3000/" className="list-group-item list-group-item-action py-2 ripple"
-            ><i className='fas fa-share fa-fw me-3'/><span>Share</span></a
+            </NavLink>
+
+            <NavLink to='/share'
+                     aria-current="true"
+                     className={({ isActive, isPending }) =>
+                       isActive
+                         ? "active01"
+                         :isPending
+                         ? "pending"
+                         : "list-group-item list-group-item-action py-2 ripple"
+                     }
+
             >
-            <a href="http://localhost:3000/" className="list-group-item list-group-item-action py-2 ripple"
-            ><i className='fas fa-comments fa-fw me-3'/><span>Feedback</span></a
+              <i className='fas fa-share fa-fw me-3'/><span>Share</span>
+            </NavLink>
+
+            <NavLink to='/feedback'
+                     aria-current="true"
+                     className={({ isActive, isPending }) =>
+                       isActive
+                         ? "active01"
+                         :isPending
+                         ? "pending"
+                         : "list-group-item list-group-item-action py-2 ripple"
+                     }
+
             >
+              <i className='fas fa-comments fa-fw me-3'/><span>Feedback</span>
+            </NavLink>
+
             <NavLink to='#'
                      aria-current="true"
                      className="pending"
