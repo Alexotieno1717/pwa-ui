2b323bbbb1c52f91a902aff07e24e0687b1bf3de
Alexotieno1717 <alexotieno900@gmail.com>
2022-10-25 12:02:02 +0300

    Added font awesome and link to the social media platform

--- src/components/support/Support.jsx
+++ src/components/support/Support.jsx
@@ -1,9 +1,13 @@
 import SideBar from '../Navigation/sidebar/SideBar';
 import Navbar from '../Navigation/Navbar/Navbar';
 import React from 'react';
+import './Support.css';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 
 
 function Support() {
+
 
   return(
     <div className="container-fluid">
@@ -17,7 +21,45 @@ function Support() {
       </header>
       {/*Main layout*/}
       <main>
-        My Team
+        <div className="container pt-5">
+          <div className='container'>
+            <div className='row'>
+              <div className='col-md-1'/>
+              <div className='col-md-8'>
+                <div className='myTeamContent pt-5'>
+                  <h4>Support</h4>
+                  <div className='supportBackground'>
+                    <h3>Need Help?</h3>
+                    <p>Give us a call at :
+                      <span className='phoneIcon'>
+                        <br/>
+                        <i className='fa fa-phone-square'> +254 794 583 651 </i>
+                        <br/>
+                        <i className='fa fa-phone-square'> +254 783 397 397 </i>
+                      </span>
+                    </p>
+
+                    <p>or reach out to us via our socials</p>
+                    <div>
+                      <a href='https://wa.me/254794583651' target='_blank' rel="noopener noreferrer">
+                        <i className='fab fa-whatsapp-square socialIcons'/>
+                      </a>
+                      <a href='https://www.instagram.com/mswali_ke/' target='_blank' rel="noopener noreferrer">
+                        <i className='fab fa-instagram-square socialIcons'/>
+                      </a>
+                      <a href='https://www.twitter.com/mswali_ke/' target='_blank' rel="noopener noreferrer">
+                        <i className='fab fa-twitter-square socialIcons'/>
+                      </a>
+                    </div>
+                  </div>
+
+
+                </div>
+              </div>
+              <div className='col-md-1'/>
+            </div>
+          </div>
+        </div>
       </main>
 
     </div>
