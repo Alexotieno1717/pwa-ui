678dc334329951f31be9edd4e9690751ffc14c2f
Alexotieno1717 <alexotieno900@gmail.com>
2022-11-01 16:19:44 +0300

    Created Questions folder

---
+++ src/components/Questions/Questions.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Questions.css';
+
+function Questions() {
+
+  
+
+  return (
+    <div className='container-fluid' id='questionsBackground'>
+      <div className='container'>
+        <div className='row'>
+          <div className="col-md-3" />
+          <div className="col-md-6">
+            <h2>Questions</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A facere recusandae vitae, libero soluta ullam non totam! Mollitia quos culpa voluptate voluptas fugiat, exercitationem iste aliquam sapiente vel porro quidem omnis nihil aliquid repellendus placeat quisquam alias minus consequuntur blanditiis distinctio eos autem rem doloremque. Obcaecati eaque molestiae, dolorum nesciunt inventore temporibus perspiciatis consequatur unde excepturi dolorem ducimus laudantium commodi cumque odit, ipsam dolore id impedit iusto nihil debitis aspernatur qui ipsum sequi ut. Repellendus aliquam facilis error non odit, asperiores rem! Suscipit accusamus eum, blanditiis esse fugiat atque quae nemo ex reiciendis harum soluta quo sint odio aspernatur dolorem.</p>
+          </div>
+          <div className="col-md-3" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Questions
