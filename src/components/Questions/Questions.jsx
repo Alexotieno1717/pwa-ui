43791efe113abbc008914a7bd3beb1db97384036
Alexotieno1717 <alexotieno900@gmail.com>
2022-11-08 11:44:34 +0300

    Used context to fetch api questions

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GameplayContext } from '../../context/gameplayContext';
+import useGameplay from '../../hooks/useGameplay';
 import './Questions.css';
 
 function Questions() {
+
+  const {sessionId} = useContext(GameplayContext)
+
+  console.log(sessionId)
 
   
 
@@ -10,9 +16,45 @@ function Questions() {
       <div className='container'>
         <div className='row'>
           <div className="col-md-3" />
-          <div className="col-md-6">
-            <h2>Questions</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A facere recusandae vitae, libero soluta ullam non totam! Mollitia quos culpa voluptate voluptas fugiat, exercitationem iste aliquam sapiente vel porro quidem omnis nihil aliquid repellendus placeat quisquam alias minus consequuntur blanditiis distinctio eos autem rem doloremque. Obcaecati eaque molestiae, dolorum nesciunt inventore temporibus perspiciatis consequatur unde excepturi dolorem ducimus laudantium commodi cumque odit, ipsam dolore id impedit iusto nihil debitis aspernatur qui ipsum sequi ut. Repellendus aliquam facilis error non odit, asperiores rem! Suscipit accusamus eum, blanditiis esse fugiat atque quae nemo ex reiciendis harum soluta quo sint odio aspernatur dolorem.</p>
+          <div className="col-md-6" id='questionSmallScreenBackground'>
+            <div className="row">
+              {/* Logo and Sound Icon */}
+              <div className="col- col-md-8">
+                <img className='questionsLogo' src={process.env.PUBLIC_URL+"/img/logos/mSwali-cyan.png"}  alt='mswali logo' />
+              </div>
+              <div className="col- col-md-4">
+                <i className='fas fa-music pt-4'>Sound Icon</i>
+              </div>
+
+              {/* Questions Timer */}
+              <div className="progress timer bg-success p-3 mt-2">
+                <div className="progress-bar text-white"></div>
+                <i className='fas fa-clock'> Timer Icon</i>
+              </div>
+
+              {/* Question image */}
+              <div className="questionImg">
+                <img className='mt-3' src="https://upload.wikimedia.org/wikipedia/commons/e/e8/Serengeti_sunset-1001.jpg" alt="Serengeti" />
+              </div>
+            </div>
+
+            {/* Question Text */}
+            <h5 className='text-center pt-3'>Serengeti National Park is in which country?</h5>
+
+            {/* Questions Choices */}
+            <div className="questionChoices">
+              <h5 className='pt-2 pl-4 '>A. Uganda</h5>
+            </div>
+            <div className="questionChoices">
+              <h5 className='pt-2 pl-4 '>B. Kenya</h5>
+            </div>
+            <div className="questionChoices">
+              <h5 className='pt-2 pl-4 '>C. Tanzania</h5>
+            </div>
+            <div className="questionChoices">
+              <h5 className='pt-2 pl-4 '>D. Rwanda</h5>
+            </div>
+
           </div>
           <div className="col-md-3" />
         </div>
