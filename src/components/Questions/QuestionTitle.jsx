fd14ed0ab297e9ceee3b4a16f8e355bb0487bc11
Alexotieno1717 <alexotieno900@gmail.com>
2023-02-02 15:41:57 +0300

    Created component

---
+++ src/components/Questions/QuestionTitle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function QuestionTitle({questions}) {
+  return (
+    <div>
+        <h5 className='text-center pt-3'>{questions}</h5>
+    </div>
+  )
+}
+
+export default QuestionTitle
